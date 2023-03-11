0752525508c8fb564fe04846bcfedcf6cd559d0b
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-11 12:03:37 +0100

    fixing bugs

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ use Services\Router;
 
 // Autoloader function
 spl_autoload_register(function ($class) {
-    require_once __DIR__ . '/../' . str_replace("\\", "/", $class) . '.php';
+    require_once __DIR__ . '/' . str_replace("\\", "/", $class) . '.php';
 });
 
 // Router should handle request
