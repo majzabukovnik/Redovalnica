6594175ef97585f46ccde20b3e1937bd7fc9745a
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-11 11:40:29 +0100

    adding code for parent model

--- Models/ParentModel.php
+++ Models/ParentModel.php
@@ -4,5 +4,14 @@ namespace Models;
 
 abstract class ParentModel
 {
-
+    private string $DBHOST = 'localhost';
+    private string $DBUSER = 'root';
+    private string $DBPASS = '';
+    private string $DBNAME = 'redovalnica';
+    protected function OpenCon(){
+        return (new mysqli($this->DBHOST, $this->DBUSER, $this->DBPASS, $this->DBNAME));
+    }
+    protected function CloseCon($conn): void{
+        $conn->close();
+    }
 }
