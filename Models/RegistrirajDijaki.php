0aaa4c1a0099fc49c293d1c7f1bf142e4a1575da
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 14:44:53 +0100

    optimizing

--- Models/RegistrirajDijaki.php
+++ Models/RegistrirajDijaki.php
@@ -4,29 +4,6 @@ namespace Models;
 
 class RegistrirajDijaki extends ParentModel
 {
-    public function getClassData(): array{
-        $conn = $this->OpenCon();
-
-        if(!$conn){
-            return ['Conncetion was not successful!'];
-        }
-
-        $query = 'SELECT id_razreda FROM Razredi';
-        $result = mysqli_query($conn, $query);
-
-        if(!$result){
-            return ['Error executing query!'];
-        }
-
-        $data = [];
-        while ($row = $result->fetch_assoc()) {
-            $data[] = $row;
-        }
-
-        $this->CloseCon($conn);
-
-        return $data;
-    }
     public function saveStudentData(): array{
         $err = [];
         $conn = $this->OpenCon();
