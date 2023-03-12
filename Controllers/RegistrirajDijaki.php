e745ece4e0131657aaa20a0f40aa65ad6adfbaeb
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 14:11:16 +0100

    delujoča registracija dijaka

--- Controllers/RegistrirajDijaki.php
+++ Controllers/RegistrirajDijaki.php
@@ -5,12 +5,29 @@ use Models\RegistrirajDijaki as RegisterModel;
 
 class RegistrirajDijaki extends ParentController
 {
-    public function showForm(): void{
-        $model = new RegisterModel();
-        $razredi = $model->getClassData();
+    protected RegisterModel $model;
+    public function __construct(){
+        $this->model = new RegisterModel();
+    }
+    public function showForm(array $err = []): void{
+        $razredi = $this->model->getClassData();
         require_once __DIR__ . '/../views/html/RegistrirajDijaki.php';
     }
     public function processRegisterData(): void{
-        
+        $err = $this->findErrors();
+
+        if(!empty($err)){
+            $this->showForm($err);
+            return;
+        }
+
+        try{
+            $this->model->saveStudentData();
+        }
+        catch (\mysqli_sql_exception $e){
+            echo "There was an error with database!";
+            sleep(5);
+            header('domov');
+        }
     }
 }
