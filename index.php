b50a1acacb227159763d68d34aff495070c4d636
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 12:15:51 +0100

    updating autoloader

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ use Services\Router;
 
 // Autoloader function
 spl_autoload_register(function ($class) {
-    require_once __DIR__ . '/' . str_replace("\\", "/", $class) . '.php';
+    require_once __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
 });
+
 
 // Router should handle request
 $router = new Router();
