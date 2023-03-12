39734f9ab777dd168d0d57c7e058b289c2bb8671
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 14:22:54 +0100

    adding method for getting all classes

--- Models/ParentModel.php
+++ Models/ParentModel.php
@@ -14,4 +14,27 @@ abstract class ParentModel
     protected function CloseCon($conn): void{
         $conn->close();
     }
+    public function getClassData(): array{
+        $conn = $this->OpenCon();
+
+        if(!$conn){
+            return ['Conncetion was not successful!'];
+        }
+
+        $query = 'SELECT id_razreda FROM Razredi';
+        $result = mysqli_query($conn, $query);
+
+        if(!$result){
+            return ['Error executing query!'];
+        }
+
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        $this->CloseCon($conn);
+
+        return $data;
+    }
 }
