8f4b259f0207e8ac7c924a3798ae2fbe10cdaabf
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 14:44:36 +0100

    adding controller for registering teacher

---
+++ Controllers/RegistrirajUcitelji.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Controllers;
+
+use Models\RegistrirajUcitelji as RegisterModel;
+
+class RegistrirajUcitelji extends ParentController
+{
+    protected RegisterModel $model;
+    public function __construct(){
+        $this->model = new RegisterModel();
+    }
+    public function showForm(array $err = []): void{
+        $razredi = $this->model->getClassData();
+        require_once __DIR__ . '/../views/html/RegistrirajProfesorji.php';
+    }
+    public function processRegisterData(): void{
+        $err = $this->findErrors();
+
+        if(!empty($err)){
+            $this->showForm($err);
+            return;
+        }
+
+        try{
+            $this->model->saveTeacherData();
+        }
+        catch (\mysqli_sql_exception $e){
+            echo "There was an error with database!";
+            sleep(2);
+            header('domov');
+        }
+    }
+}
