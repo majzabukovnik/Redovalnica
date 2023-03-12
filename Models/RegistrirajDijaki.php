821169608cba9e9add4a21ac804b8f1a7bc1dc3e
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 14:11:39 +0100

    working model for writing to database

--- Models/RegistrirajDijaki.php
+++ Models/RegistrirajDijaki.php
@@ -27,4 +27,31 @@ class RegistrirajDijaki extends ParentModel
 
         return $data;
     }
+    public function saveStudentData(): array{
+        $err = [];
+        $conn = $this->OpenCon();
+
+        if(!$conn){
+            return ['Conncetion was not successful!'];
+        }
+
+        $prepare = $conn->prepare("INSERT INTO dijaki (id_dijaka, ime, priimek, naslov, telefonska_stevilka, email, geslo, datum_rojstva, razred) VALUES (?,?,?,?,?,?,?,?,?)");
+        $geslo = password_hash($_POST['id_dijaka'] . $_POST['razred'], PASSWORD_BCRYPT);
+        $prepare->bind_param('sssssssss',
+        $_POST['id_dijaka'],
+            $_POST['ime'],
+            $_POST['priimek'],
+            $_POST['naslov'],
+            $_POST['telefonska_stevilka'],
+            $_POST['email'],
+            $geslo,
+            $_POST['datum_rojstva'],
+            $_POST['razred']
+        );
+        $prepare->execute();
+
+        $this->CloseCon($conn);
+
+        return $err;
+    }
 }
