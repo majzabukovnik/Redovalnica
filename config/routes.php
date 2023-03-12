71c007bea6347e4ca1510fa1f1ce121aea6d0e17
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 17:37:18 +0100

    adding routes for log in page

--- config/routes.php
+++ config/routes.php
@@ -4,18 +4,23 @@ use Controllers\ParentController;
 use Controllers\Index;
 use Controllers\RegistrirajDijaki;
 use Controllers\RegistrirajUcitelji;
+use Controllers\Prijava;
 
 
 return [
     '/Redovalnica/' => [
-        'GET' => [RegistrirajDijaki::class, "showForm"]
+        'GET' => [RegistrirajDijaki::class, 'showForm']
     ],
     '/Redovalnica/registracijaDijaka/' => [
-        'GET' => [RegistrirajDijaki::class, "showForm"],
-        'POST' => [RegistrirajDijaki::class, "processRegisterData"]
+        'GET' => [RegistrirajDijaki::class, 'showForm'],
+        'POST' => [RegistrirajDijaki::class, 'processRegisterData']
     ],
     '/Redovalnica/registracijaUcitelja/' => [
-        'GET' => [RegistrirajUcitelji::class, "showForm"],
-        'POST' => [RegistrirajUcitelji::class, "processRegisterData"]
+        'GET' => [RegistrirajUcitelji::class, 'showForm'],
+        'POST' => [RegistrirajUcitelji::class, 'processRegisterData']
+    ],
+    '/Redovalnica/prijava' => [
+        'GET' => [Prijava::class, 'showForm'],
+        'POST' => [Prijava::class, 'processLogin']
     ]
 ];
