fa32ece8c61f2a825853c08de767ab6312ed67b3
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 19:40:03 +0100

    bug fixes

--- Controllers/Index.php
+++ Controllers/Index.php
@@ -5,6 +5,9 @@ namespace Controllers;
 class Index extends ParentController
 {
     public function showForm(): void{
+        if(parse_url($_SERVER['REQUEST_URI'])['path'] === '/Redovalnica/'){
+            header("Location: /Redovalnica/domov/");
+        }
         require_once __DIR__ . '/../views/html/partials/navBar.php';
     }
 }
