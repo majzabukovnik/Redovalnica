7b6d5d418fcc6187c685ec11de3b0238d0905f39
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 14:10:51 +0100

    adding general function for finding errors in submitted forms

--- Controllers/ParentController.php
+++ Controllers/ParentController.php
@@ -5,5 +5,14 @@ use Models\ParentModel;
 
 abstract class ParentController
 {
+    protected function findErrors(): array{
+        $err = [];
+        foreach($_POST as $key=>$value){
+            if(empty($value)){
+                $err[] = 'Obvezno vnesite vrednost za ' . $key . '!';
+            }
+        }
 
+        return $err;
+    }
 }
