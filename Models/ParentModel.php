e0d021e802f6a7dde13b0a584193bd04a0050483
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-12 12:16:10 +0100

    fixing bug for establishing connection

--- Models/ParentModel.php
+++ Models/ParentModel.php
@@ -9,7 +9,7 @@ abstract class ParentModel
     private string $DBPASS = '';
     private string $DBNAME = 'redovalnica';
     protected function OpenCon(){
-        return (new mysqli($this->DBHOST, $this->DBUSER, $this->DBPASS, $this->DBNAME));
+        return (new \mysqli($this->DBHOST, $this->DBUSER, $this->DBPASS, $this->DBNAME));
     }
     protected function CloseCon($conn): void{
         $conn->close();
