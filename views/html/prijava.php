98c30344839b7550a6131476afb6513da27e0e3a
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-13 21:57:49 +0100

    adding html structure

--- views/html/prijava.php
+++ views/html/prijava.php
@@ -7,24 +7,31 @@
 </head>
 
 <body>
-<?php require_once __DIR__ . '/partials/navBar.php'; ?>
-<form method="POST" action="">
+<div class="navBar"><?php require_once __DIR__ . '/partials/navBar.php'; ?></div>
+<div class="main">
+    <div class="leftEdge"><p class="content"></p></div>
+    <div class="content">
+        <form method="POST" action="">
 
-    <label for="email">Email: </label>
-    <input name="email" type="text"><br>
+            <label for="email">Email: </label>
+            <input name="email" type="text"><br>
 
-    <label for="geslo">Geslo: </label>
-    <input name="geslo" type="password"><br>
+            <label for="geslo">Geslo: </label>
+            <input name="geslo" type="password"><br>
 
-    <input type="submit" value="Prijavi se">  <br>
+            <input type="submit" value="Prijavi se">  <br>
 
-    <div>
-        <ul>
-            <?php foreach ($err as $error) : ?>
-                <li><?php echo $error ?></li>
-            <?php endforeach; ?>
-        </ul>
+            <div>
+                <ul>
+                    <?php foreach ($err as $error) : ?>
+                        <li><?php echo $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
     </div>
+    <div class="rightEdge"></div>
+</div>
+<div class="footer"><?php //require_once "partials/footer.php"?></div>
 </form>
 </body>
 
