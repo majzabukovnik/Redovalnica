7e2826e05a4f527f92e1c19faa04b49f1ed6985e
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-13 21:57:58 +0100

    adding code for navBar

--- views/html/partials/navBar.php
+++ views/html/partials/navBar.php
@@ -1,1 +1,24 @@
-<h1>navBar</h1>
+<style>
+    <?php require_once __DIR__ . '/../../css/mainCSS.css';
+    require_once __DIR__ . '/../../css/navBar.css';
+    require_once __DIR__ . '/../../css/form.css'; ?>
+</style>
+<nav>
+    <div class="left">
+        <a href="#" class="logo-link"><span class="logo">Redovalnica</span></a>
+    </div>
+    <div class="middle">
+        <a href="#" class="nav-link">Domov</a>
+        <a href="#" class="nav-link">Ocene</a>
+        <a href="#" class="nav-link">Izostanki</a>
+        <a href="#" class="nav-link">Kontakt</a>
+    </div>
+    <div class="right">
+        <a href="#" class="user-profile-link">
+            <div class="user-profile">
+                <img src="profile.webp" alt="User profile picture">
+                <span class="username">John Doe</span>
+            </div>
+        </a>
+    </div>
+</nav>
