78b2a42fc54e7e113b60f3dba496c40a5092a2ec
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-14 21:11:52 +0100

    adding 404page response

---
+++ views/html/404page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Prijava</title>
+</head>
+
+<body>
+<div class="navBar"><?php require_once __DIR__ . '/partials/navBar.php'; ?></div>
+<div class="main">
+    <div class="leftEdge"><p class="content"></p></div>
+    <div class="content">
+        <h1 class="title">Oops! Stran ne obstaja!</h1>
+        <p class="subtitle">Preverite vpisani naslov ali uporabite možnost iskanja. Hvala!</p>
+        <a class="btn" href="/Redovalnica/domov">Go back to homepage</a>
+    </div>
+    <div class="rightEdge"></div>
+</div>
+<div class="footer"><?php //require_once "partials/footer.php"?></div>
+</form>
+</body>
+
+</html>
