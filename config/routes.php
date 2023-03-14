f2a426b5451f93909c98dd4ee7ba05e4eac1ba32
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-14 22:17:12 +0100

    adding routes for regestering class

--- config/routes.php
+++ config/routes.php
@@ -6,15 +6,25 @@ use Controllers\RegistrirajDijaki;
 use Controllers\RegistrirajUcitelji;
 use Controllers\Prijava;
 use Controllers\Odjava;
+use Controllers\RegistrirajRazred;
 
 
 return [
+    //normal user pages
     '/Redovalnica/' => [
         'GET' => [Index::class, 'showForm']
     ],
     '/Redovalnica/domov/'=>[
         'GET' => [Index::class, 'showForm']
     ],
+    '/Redovalnica/prijava/' => [
+        'GET' => [Prijava::class, 'showForm'],
+        'POST' => [Prijava::class, 'processLogin']
+    ],
+    '/Redovalnica/odjava/' => [
+        'GET' => [Odjava::class, 'odjaviSe']
+    ],
+    //admin pages
     '/Redovalnica/registracijaDijaka/' => [
         'GET' => [RegistrirajDijaki::class, 'showForm'],
         'POST' => [RegistrirajDijaki::class, 'processRegisterData']
@@ -23,11 +33,8 @@ return [
         'GET' => [RegistrirajUcitelji::class, 'showForm'],
         'POST' => [RegistrirajUcitelji::class, 'processRegisterData']
     ],
-    '/Redovalnica/prijava/' => [
-        'GET' => [Prijava::class, 'showForm'],
-        'POST' => [Prijava::class, 'processLogin']
-    ],
-    '/Redovalnica/odjava/' => [
-        'GET' => [Odjava::class, 'odjaviSe']
+    '/Redovalnica/registrirajRazred/' => [
+        'GET' => [RegistrirajRazred::class, 'showForm'],
+        'POST' => [RegistrirajRazred::class, 'processRegisterData']
     ]
 ];
