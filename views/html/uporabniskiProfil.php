89f26f5ff9b5d1c00497a2cf0899238b190b8088
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-15 20:31:52 +0100

    creating form for displaying user data and changing password

--- views/html/uporabniskiProfil.php
+++ views/html/uporabniskiProfil.php
@@ -12,13 +12,34 @@
     <div class="leftEdge"><p class="content"></p></div>
     <div class="content">
         <h1>Uporabniške nastavitve</h1>
-        <form method="POST" action="">
 
+        <form method="POST" action="">
+            <p class="osebniPodatki">Ime: <?php echo $_SESSION['ime']; ?></p>
+            <p class="osebniPodatki">Priimek: <?php echo $_SESSION['priimek']; ?></p>
+            <p class="osebniPodatki">Email: <?php echo $_SESSION['email']; ?></p>
+            <p class="osebniPodatki">Naslov: <?php echo $_SESSION['naslov']; ?></p>
+            <p class="osebniPodatki">Telefonska številka: <?php echo $_SESSION['telefonska_stevilka']; ?></p>
+            <p class="osebniPodatki">Datum rojstva: <?php echo $_SESSION['datum_rojstva']; ?></p>
+            <p class="osebniPodatki">Matični razred: <?php echo $_SESSION['razrednik']; ?></p>
+            <p class="osebniPodatki" style="font-weight: bold">Za spreminjanje zgornjih podatkov se prosimo obrnite na administratorja!</p>
+            <br>
+            <h2>Spremeni geslo</h2> <br>
+            <label for="staroGeslo">Staro geslo: </label>
+            <input name="staroGeslo" type="password"><br>
+
+            <label for="novoGeslo1">Novo geslo: </label>
+            <input name="novoGeslo1" type="password"><br>
+
+            <label for="staroGeslo">Ponovno vnesite novo geslo: </label>
+            <input name="novoGeslo2" type="password"><br>
+
+            <input type="submit">
         </form>
     </div>
+
     <div class="rightEdge"></div>
 </div>
-<div class="footer"><?php //require_once "partials/footer.php"?></div>
+<div class="footer"><?php require_once "partials/footer.php"?></div>
 </form>
 </body>
 
