c34f9271a8b6565d9779ba25d892deb208451103
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-03-15 20:30:44 +0100

    making it possible to view this page only if logged in

--- Controllers/UporabniskiProfil.php
+++ Controllers/UporabniskiProfil.php
@@ -5,10 +5,10 @@ namespace Controllers;
 class UporabniskiProfil
 {
     public function showForm(array $err = []): void{
-        /*if(!isset($_SESSION['ime'])){
+        if(!isset($_SESSION['ime'])){
             header('Location: /Redovalnica/prijava/');
             exit();
-        }*/
+        }
         require_once __DIR__ . '/../views/html/UporabniskiProfil.php';
     }
 }
