cc3f5cfb5556774880970b9a549d83f1f3e2202f
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-05-11 19:56:52 +0200

    adding useless complex query

--- Models/ParentModel.php
+++ Models/ParentModel.php
@@ -142,4 +142,37 @@ abstract class ParentModel
 
         return $data;
     }
+
+    public function complexQuery(): array {
+        $conn = $this->openCon();
+
+        if (!$conn) {
+            return ['Connection was not successful!'];
+        }
+
+        $query = 'SELECT Ucitelji.ime, Ucitelji.priimek, Predmet.id_predmeta, COUNT(Ocene.id_ocene) AS st_ocen
+                    FROM Ucitelji
+                    JOIN Uci ON Ucitelji.id_ucitelja = Uci.id_ucitelja
+                    JOIN Predmet ON Uci.id_predmeta = Predmet.id_predmeta
+                    LEFT JOIN Ocene ON Uci.id_uci = Ocene.id_uci
+                    GROUP BY Ucitelji.id_ucitelja, Predmet.id_predmeta
+                    HAVING COUNT(Ocene.id_ocene) > 0';
+
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if (!$result) {
+            return ['Error executing query!'];
+        }
+
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        $this->closeCon($conn);
+
+        return $data;
+    }
 }
